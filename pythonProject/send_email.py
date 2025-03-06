b71da44c370ee235249c6ea05046e13b35653098
Ashmith AMddala <ashmith.maddala@gmail.com>
2025-03-06 23:14:18 +0530

    changed data, added titles to the Home, created send_email to send email from the form to gmail.

---
+++ pythonProject/send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "ashmith.maddala@gmail.com"
+    password = os.getenv("GMAIL_PASSWORD")
+    receiver = "ashmith.maddala@gmail.com"
+
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
