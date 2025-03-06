65b5693922717f2da576089da9415528d9461549
Ashmith AMddala <ashmith.maddala@gmail.com>
2025-03-06 21:36:35 +0530

    Add navigation menu, and Contact_Us page.

--- pythonProject/Home.py
+++ pythonProject/Home.py
@@ -5,7 +5,8 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/photo.png",)
+
 
 with col2:
     st.title("Ashmith Maddala")
@@ -18,3 +19,9 @@ with col2:
     
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+
+st.write(content2)
